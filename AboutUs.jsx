a28f84589c721d5ce5bdd14f49b697811829f2f2
Avril044 <150882105+Avril044@users.noreply.github.com>
2026-09-03 10:13:29 -0600

    Add AboutUs component for company introduction

---
+++ AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function AboutUs(){
+ return (
+  <div>
+   <h1>About Us</h1>
+   <p>Welcome to Paradise Nursery, where green meets serenity!</p>
+   <p>At Paradise Nursery, we are passionate about bringing nature closer to you. We are a company dedicated to houseplants.</p>
+  </div>
+ )
+}
+export default AboutUs;
