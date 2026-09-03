89d79bdf1d3e28c31d65cc05551dbdf24a4c8854
Avril044 <150882105+Avril044@users.noreply.github.com>
2026-09-03 10:35:22 -0600

    Enhance AboutUs component with new content and styling
    Updated AboutUs component to include additional information and class name.

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-function AboutUs(){
- return (
-  <div>
-   <h1>About Us</h1>
-   <p>Welcome to Paradise Nursery, where green meets serenity!</p>
-   <p>At Paradise Nursery, we are passionate about bringing nature closer to you. We are a company dedicated to houseplants.</p>
-  </div>
- )
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1>About Us</h1>
+      <p>Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p>At Paradise Nursery, we are passionate about bringing nature closer to you.</p>
+      <p>We offer a wide variety of houseplants to make your home greener.</p>
+    </div>
+  );
 }
 export default AboutUs;
